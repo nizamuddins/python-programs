ac8f770492958a68a0f60fbc3af58a9957779fc1
nizamuddins <uddinmdnizam61@gmail.com>
2022-11-26 01:02:20 +0530

    python

--- index17.py
+++ index17.py
@@ -1,2 +1,17 @@
-num = 153;
+import math;
 
+num = '153';
+length = len(num)
+temp = int(num);
+sum = 0;
+
+while temp!=0:
+    rem = temp%10;
+    val = rem ** length;
+    sum = sum+val;
+    temp = math.floor(temp/10);
+
+if sum == int(num):
+    print("It is pallindrome");
+else:
+    print("Not a pallindrome")        
