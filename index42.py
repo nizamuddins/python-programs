5eae8d49aa5456077dfa643b9491ece80a4e1fc9
nizamuddins <uddinmdnizam61@gmail.com>
2022-12-18 00:06:06 +0530

    python

--- index42.py
+++ index42.py
@@ -1,11 +1,40 @@
 #  By has_key()
+# tuples
+defTuples = ("apple","mango","banana");
+for i in defTuples:
+    print(i)
+print(defTuples[2])
+if "apple" in defTuples:
+    print("true")
 
-class User():
-    def __init__(self):
-        self.name = "nizam";
-        
-obj = User();
+# set
+newset = {1,3,2,4,7,4,3,6,7,4,};
+for i in newset:
+    print(i)
+print(newset);
+newset.add(0);
+print(newset);
+if 2 in newset:
+    print("true")
 
-if 'name' in obj:
-    print('True')
+
+# dictionary
+defDic = {
+    "name":"nizam",
+    "age":21,
+    "work":"student"
+};
+
+print(defDic);
+print(defDic['name']);
+defDic['name'] = "mohd";
+print(defDic['name']);
+
+for i in defDic:
+    print(f'{i}:{defDic[i]}')
+
+if 'name' in defDic:
+    print("true")
+
+
 
